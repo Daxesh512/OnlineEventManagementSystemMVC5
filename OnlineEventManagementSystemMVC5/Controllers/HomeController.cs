d5c8a71ce8fd7432ab7e983949a4f4c301159c54
Daxesh512 <99954315+Daxesh512@users.noreply.github.com>
2025-11-19 23:55:43 +0530

    EventMgmt

---
+++ OnlineEventManagementSystemMVC5/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using OnlineEventManagementSystemMVC5.Models;
+
+
+namespace OnlineEventManagementSystemMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registartion(TblUser r1)
+        {
+            using (EventDTOEntities db = new EventDTOEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.TblUsers.Add(r1);
+                    db.SaveChanges();
+                    ViewBag.message("Registration Sucessfully");
+                    ModelState.Clear();
+                }
+            }
+            return View(r1);
+        }
+        [HttpPost]
+        public ActionResult Login(TblUser r1)
+        {
+            using (EventDTOEntities db = new EventDTOEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    var log = db.TblUsers.Where(a => a.userName.Equals(r1.userName) && a.pass.Equals(r1.pass)).FirstOrDefault();
+                    if (log != null)
+                    {
+                        return RedirectToAction("Contact");
+                    }
+
+                    ViewBag.message("Login Sucessfully");
+                    ModelState.Clear();
+                }
+            }
+            return View(r1);
+        }
+
+
+    }
+}
---
+++ OnlineEventManagementSystemMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineEventManagementSystemMVC5.MvcApplication" Language="C#" %>
